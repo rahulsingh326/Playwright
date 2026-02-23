48d896cdc30b05c3fb558a6156c3ba4c5d2323f3
rahulsingh326 <job.rahuslsingh@gmail.com>
2026-02-23 21:57:14 +0530

    Update code

--- Chapter1/prog3.js
+++ Chapter1/prog3.js
@@ -1,1 +1,13 @@
-//test push from vscode copilot 
+let mark = 59;
+
+if (mark >= 90) {
+    console.log("Grade A");
+} else if (mark >= 80) {
+    console.log("Grade B");
+} else if (mark >= 70) {
+    console.log("Grade C");
+} else if (mark >= 60) {
+    console.log("Grade D");
+} else {
+    console.log("Grade F");
+}
