f207d896dd11e99b83a8a167459e3999ee6acd24
rahulsingh326 <job.rahuslsingh@gmail.com>
2026-02-28 08:16:51 +0530

    chore: commit workspace files

---
+++ Chapter1/prog5.js
@@ -0,0 +1,8 @@
+ let status1 = 200;
+  if (status1 === 200) 
+    { console.log("OK"); 
+
+    } else if (status1 === 404) 
+        { console.log("Not Found"); 
+            
+        } else { console.log("Other"); } 
